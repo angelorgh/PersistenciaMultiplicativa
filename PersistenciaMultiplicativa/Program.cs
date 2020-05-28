3f80a8f500b8e54f3d7c52ee0640f60a6cd80bc9
AngeloRGH <renatoangelo4@gmail.com>
2020-05-27 20:38:07 -0400

    Add project files.

---
+++ PersistenciaMultiplicativa/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PersistenciaMultiplicativa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculatePersistence(999999));
+        }
+
+        public static int CalculatePersistence(int num)
+        {
+            string numasString = Convert.ToString(num);
+            int counter = 0;
+
+            while (numasString.Length > 1)
+            {
+                int temp = (int)Char.GetNumericValue(numasString[0]);
+
+                for (int i = 0; i < numasString.Length - 1; i++)
+                {
+                    temp *= (int)Char.GetNumericValue(numasString[i + 1]);
+                }
+
+                numasString = Convert.ToString(temp);
+
+                counter++;
+
+            }
+
+            return counter;
+        }
+    }
+}
